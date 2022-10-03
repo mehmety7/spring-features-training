42ca2e3df89d8b7e443ac9a90b521297d808456e
mehmety7 <mehmetyazici.3407@gmail.com>
2022-10-03 12:07:00 +0300

    add remote h2 database conf

--- src/main/java/com/trial/springaop/configuration/H2ServerConfig.java
+++ src/main/java/com/trial/springaop/configuration/H2ServerConfig.java
@@ -3,14 +3,15 @@ package com.trial.springaop.configuration;
 import org.h2.tools.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 import java.sql.SQLException;
 
 @Configuration
 public class H2ServerConfig {
 
-
     @Bean(initMethod = "start", destroyMethod = "stop")
+    @Profile("test1")
     public Server inMemoryH2DatabaseServer() throws SQLException {
         return Server.createTcpServer(
                 "-tcp", "-tcpAllowOthers", "-tcpPort", "9090"
