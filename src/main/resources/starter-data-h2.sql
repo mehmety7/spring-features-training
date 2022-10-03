60b09f1bba2dbcc2e0e9bf4becfd8fad05c8fcaf
mehmety7 <mehmetyazici.3407@gmail.com>
2022-10-03 12:06:02 +0300

    add new profile

---
+++ src/main/resources/starter-data-h2.sql
@@ -0,0 +1,8 @@
+insert into trial.book (name,isbn,number) values ( 'book1', '1', 1 );
+insert into trial.book (name,isbn,number) values ( 'book2', '2', 1 );
+insert into trial.book (name,isbn,number) values ( 'book3', '3', 1 );
+insert into trial.book (name,isbn,number) values ( 'book4', '4', 1 );
+insert into trial.book (name,isbn,number) values ( 'book5', '5', 1 );
+insert into trial.book (name,isbn,number) values ( 'book6', '6', 1 );
+insert into trial.book (name,isbn,number) values ( 'book7', '7', 1 );
+insert into trial.book (name,isbn,number) values ( 'book8', '8', 1 );
--- src/main/resources/starter-schema-h2.sql
+++ src/main/resources/starter-schema-h2.sql
@@ -4,5 +4,6 @@ create table if not exists trial.book (
     id INTEGER not null auto_increment,
     name varchar(255),
     isbn varchar(255),
+    number INT CHECK (number IN (1,2)),
     primary key (id)
 );
