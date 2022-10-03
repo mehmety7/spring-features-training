b679a89e9893ec74891700e6f35d24350baae784
mehmety7 <mehmetyazici.3407@gmail.com>
2022-10-03 12:06:16 +0300

    add new profile

--- src/main/resources/data-h2.sql
+++
@@ -1,8 +0,0 @@
-insert into trial.book (name,isbn) values ( 'book1', '1' );
-insert into trial.book (name,isbn) values ( 'book2', '2' );
-insert into trial.book (name,isbn) values ( 'book3', '3' );
-insert into trial.book (name,isbn) values ( 'book4', '4' );
-insert into trial.book (name,isbn) values ( 'book5', '5' );
-insert into trial.book (name,isbn) values ( 'book6', '6' );
-insert into trial.book (name,isbn) values ( 'book7', '7' );
-insert into trial.book (name,isbn) values ( 'book8', '8' );
